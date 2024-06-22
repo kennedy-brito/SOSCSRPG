3870c85e5ce8fcd879063f29c86b0e98b3da0054
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-06-22 17:25:02 -0300

    feat: added world class

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -6,77 +6,76 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Markup;
 
-namespace Engine.Models
+namespace Engine.Models;
+
+public class Player : INotifyPropertyChanged
 {
-    public class Player : INotifyPropertyChanged
-    {
-        private string _name ;
-        private string _characterClass ;
-        private int _hitPoints ;
-        private int _experiencePoints;
-        private int _level ;
-        private int _gold ;
-        public string Name 
-        { 
-            get{ return _name;}
-            set
-            {
-                _name = value;
-                OnPropertyChanged("Name");
-            }
-        }
-        public string CharacterClass 
-        { 
-            get{ return _characterClass;}
-            set
-            {
-                _characterClass = value;
-                OnPropertyChanged("CharacterClass");
-            }
-        } 
-        public int HitPoints 
+    private string _name ;
+    private string _characterClass ;
+    private int _hitPoints ;
+    private int _experiencePoints;
+    private int _level ;
+    private int _gold ;
+    public string Name 
+    { 
+        get{ return _name;}
+        set
         {
-            get { return _hitPoints; }
-            set 
-            {
-                _hitPoints = value;
-                OnPropertyChanged("HitPoints");
-            }
+            _name = value;
+            OnPropertyChanged("Name");
         }
-
-        public int ExperiencePoints
-        {
-            get { return _experiencePoints; }
-            set
-            {
-                _experiencePoints = value;
-                OnPropertyChanged("ExperiencePoints");
-            }
-        }
-        public int Level
-        { 
-            get {return _level;}
-            set
-            {
-                _level = value;
-                OnPropertyChanged("Level");
-            }
-        }
-        public int Gold 
-        {
-            get { return _gold; }
-            set 
-            { 
-                _gold = value;
-                OnPropertyChanged("Gold");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
     }
+    public string CharacterClass 
+    { 
+        get{ return _characterClass;}
+        set
+        {
+            _characterClass = value;
+            OnPropertyChanged("CharacterClass");
+        }
+    } 
+    public int HitPoints 
+    {
+        get { return _hitPoints; }
+        set 
+        {
+            _hitPoints = value;
+            OnPropertyChanged("HitPoints");
+        }
+    }
+
+    public int ExperiencePoints
+    {
+        get { return _experiencePoints; }
+        set
+        {
+            _experiencePoints = value;
+            OnPropertyChanged("ExperiencePoints");
+        }
+    }
+    public int Level
+    { 
+        get {return _level;}
+        set
+        {
+            _level = value;
+            OnPropertyChanged("Level");
+        }
+    }
+    public int Gold 
+    {
+        get { return _gold; }
+        set 
+        { 
+            _gold = value;
+            OnPropertyChanged("Gold");
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
 }
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -5,31 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
 
-namespace Engine.ViewModels
+namespace Engine.ViewModels;
+
+public class GameSession
 {
-    public class GameSession
+
+    public Player CurrentPlayer { get; set; }
+    public Location CurrentLocation { get; set; }
+    public GameSession()
     {
+        CurrentPlayer = new Player();
+        CurrentPlayer.Name = "Scott";
+        CurrentPlayer.CharacterClass = "Fighter";
+        CurrentPlayer.Gold = 1000;
+        CurrentPlayer.HitPoints = 10;
+        CurrentPlayer.ExperiencePoints = 0;
+        CurrentPlayer.Level = 1;
 
-        public Player CurrentPlayer { get; set; }
-        public Location CurrentLocation { get; set; }
-        public GameSession()
-        {
-            CurrentPlayer = new Player();
-            CurrentPlayer.Name = "Scott";
-            CurrentPlayer.CharacterClass = "Fighter";
-            CurrentPlayer.Gold = 1000;
-            CurrentPlayer.HitPoints = 10;
-            CurrentPlayer.ExperiencePoints = 0;
-            CurrentPlayer.Level = 1;
-
-            CurrentLocation = new Location();
-            CurrentLocation.Name = "Home";
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = -1;
-            CurrentLocation.Description = "This is your home";
-            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
-
-        }
+        CurrentLocation = new Location();
+        CurrentLocation.Name = "Home";
+        CurrentLocation.XCoordinate = 0;
+        CurrentLocation.YCoordinate = -1;
+        CurrentLocation.Description = "This is your home";
+        CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
 
     }
+
 }
