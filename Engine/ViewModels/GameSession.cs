d7fdd87048a0fa267284b1e3b8b9fb4846816d59
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-06-22 20:58:13 -0300

    feat: adding the option to move in the map

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -5,13 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
 using Engine.Factories;
+using System.ComponentModel;
 namespace Engine.ViewModels;
 
-public class GameSession
+public class GameSession : INotifyPropertyChanged
 {
+    private Location _currentLocation;
     public World CurrentWorld { get; set; }
     public Player CurrentPlayer { get; set; }
-    public Location CurrentLocation { get; set; }
+    public Location CurrentLocation { 
+        get { return _currentLocation; } 
+        set
+        {
+            _currentLocation = value;
+            OnPropertyChanged("CurrentLocation");
+        } 
+    }
     public GameSession()
     {
         CurrentPlayer = new Player();
@@ -25,7 +34,31 @@ public class GameSession
         var factory = new WorldFactory();
         CurrentWorld = factory.CreateWorld();
 
-        CurrentLocation = CurrentWorld.LocationAt(0, -1);
+        CurrentLocation = CurrentWorld.LocationAt(0, 0);
     }
 
+    public void MoveNorth()
+    {
+        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+    }
+    public void MoveWest()
+    {
+        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+    }
+    public void MoveSouth()
+    {
+        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+
+    }
+    public void MoveEast()
+    {
+        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
