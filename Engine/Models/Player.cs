aa29fe43cfb8abda9d641614cd2e57b2e5d63003
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-06-26 17:10:22 -0300

    feat: added Player Inventory

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -71,5 +72,7 @@ public class Player : BaseNotificationClass
             OnPropertyChanged(nameof(Gold));
         }
     }
+    
+    public ObservableCollection<GameItem> Inventory { get; set; } = new ();
 
 }
