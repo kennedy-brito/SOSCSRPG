7e9f6e0905f00fae7e5aea1ceab05ae509e640ec
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-06-27 19:01:19 -0300

    feat: created monster and monster factory class

---
+++ Engine/Factories/MonsterFactory.cs
@@ -0,0 +1,51 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Factories;
+
+public static class MonsterFactory
+{
+    public static Monster GetMonster(int monsterID)
+    {
+        switch(monsterID)
+        {
+            case 1:
+                Monster snake = new Monster("Snake", "Snake.png", 4, 4, 5, 1);
+
+                AddLootItem(snake, 9001, 25);
+                AddLootItem(snake, 9002, 75);
+                
+                return snake;
+            
+            case 2:
+                Monster rat = new Monster("Rat", "Rat.png", 4, 4, 5, 1);
+
+                AddLootItem(rat, 9001, 25);
+                AddLootItem(rat, 9002, 75);
+                
+                return rat;            
+            
+            case 3:
+                Monster giantSpider = new Monster("G    iantSpider", "GiantSpider.png", 4, 4, 5, 1);
+
+                AddLootItem(giantSpider, 9001, 25);
+                AddLootItem(giantSpider, 9002, 75);
+                
+                return giantSpider;
+            default:
+                throw new ArgumentException(string.Format("Monster Type '{0}' does not exist", monsterID));
+        }
+    }
+
+    private static void AddLootItem(Monster monster, int itemId, int percentage)
+    {
+        if (RandomNumberGenerator.NumberBetween(1,100) <= percentage)
+        {
+            monster.Inventory.Add(new ItemQuantity(itemId, 1));
+        }
+    }
+}
---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models;
+
+internal class Monster : BaseNotificationClass
+{
+    private int _hitPoints;
+    
+    public string Name { get; set; }
+    public string ImageName { get; set; }
+    public int MaximumHitPoints { get; set; }
+    public int HitPoints
+    {
+        get { return _hitPoints; }
+        set 
+        { 
+            _hitPoints = value;
+            OnPropertyChanged(nameof(HitPoints));
+
+        }
+    }
+
+    public int RewardExperiencePoints { get; set; }
+    public int RewardGold {  get; set; }
+
+    public ObservableCollection<ItemQuantity> Inventory{ get; set; }
+
+    public Monster(
+        string name, string imageName, 
+        int maximumHitPoints, int hitPoints,
+        int rewardExperiencePoints, int rewardGold)
+    {
+        HitPoints = hitPoints;
+        Name = name;
+        ImageName = $"pack://application:,,,/Engine/Images/Monsters/{imageName}";
+        MaximumHitPoints = maximumHitPoints;
+        RewardExperiencePoints = rewardExperiencePoints;
+        RewardGold = rewardGold;
+
+        Inventory = new ObservableCollection<ItemQuantity>();
+    }
+}
